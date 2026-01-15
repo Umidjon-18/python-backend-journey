df4980b0109d32814a57f6865c990a614a164236
Umidjon-18 <uyoqubov@texnomart.uz>
2026-01-15 14:42:36 +0500

    basic data types

---
+++ week_1/data_types.py
@@ -0,0 +1,9 @@
+num1: int = 12
+num2: float = 24.3
+word: str = "Hello"
+really: bool = True
+numbers: list[int] = [1, 2, 3]
+constant_list: tuple[int] = (1, 2, 3)
+hashmap = {"a": 12, "b": 24.4}
+hashset = {1, 2, 3, 4, 5}
+
