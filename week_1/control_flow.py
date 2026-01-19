276d2cfcb1c7d5f546a86f405671634c1b62f14f
Umidjon-18 <uyoqubov@texnomart.uz>
2026-01-19 11:59:11 +0500

    control flow in progress

---
+++ week_1/control_flow.py
@@ -0,0 +1,57 @@
+result = True
+
+if result:
+    print("Yeah, we did it")
+else:
+    print("Ooops, something went wrong")
+
+
+def get_result(target: int):
+    return 0 if target < 0 else target
+
+
+print(get_result(1))
+print(get_result(-1))
+
+values = {"a": 2, "b": 4}
+for [key, value] in values.items():
+    print(key, value)
+
+
+values = {"a", "b", "c"}
+for value in values:
+    print(value)
+
+for char in "abcdefg":
+    print(char)
+
+user_input = ""
+while user_input != "exit":
+    user_input = input("Type something: ")
+    print(f"You typed: {user_input}")
+else:
+    print("As you typed 'exit', the program quit")
+
+emails = [
+    " alice@example.org ",
+    "BOB@example.com",
+    "charlie@EXAMPLE.com",
+    "David@example.net",
+    " bob@example.com",
+    "JohnDoe@example.com",
+    "DAVID@Example.net",
+]
+
+lowered_emails = [email.strip().lower() for email in emails if "@" in email]
+print(lowered_emails)
+
+
+def count_down(number: int):
+    if number <= 0:
+        print("End")
+    else:
+        print(number)
+        count_down(number - 1)
+
+
+count_down(9)
