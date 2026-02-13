abcd38e3dacfe8e359793c1fce9638b31a6b59f0
Umidjon-18 <uyoqubov@texnomart.uz>
2026-02-13 16:59:41 +0500

    state pattern is in progress

--- week2/atm_system/atm_state.py
+++ week2/atm_system/atm_state.py
@@ -1,8 +1,29 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from atm import *
 
 
 class ATMState(ABC):
-    pass
+
+    def __init__(self, atm: ATM):
+        self.atm = atm
+
+    def insert_card(self, card):
+        return NotImplementedError
+
+    def authenticate(self, pin: str):
+        return NotImplementedError
+
+    def withdraw(self, amount: float):
+        return NotImplementedError
+
+    def deposit(self, amount: float):
+        return NotImplementedError
+
+    def check_balance(self):
+        return NotImplementedError
+
+    def eject_card(self):
+        return NotImplementedError
 
 
 class IdleState(ATMState):
