207cd676b550faacae1a9285b96510353200981a
Umidjon-18 <uyoqubov@texnomart.uz>
2026-02-20 16:54:32 +0500

    week3 in progress

---
+++ week3/garbage_collection.py
@@ -0,0 +1,8 @@
+import sys
+
+x = [1, 2, 3]
+print(sys.getrefcount(x))
+y = x
+print(sys.getrefcount(x))
+y = None
+print(sys.getrefcount(x))
---
+++ week3/lists_dicts_sets.py
@@ -0,0 +1,46 @@
+# nums = [1, 2, 3, 4]
+# print(nums * 2)
+
+# import numpy as np
+
+# nums = np.array([[1, 2], [3, 4]])
+# print(nums * 2)
+
+# nums = []
+# nums.insert(0, 1)
+# print(nums)
+
+# import array as ar
+
+# a = ar.array("i", [1, 2, 3, 4])
+# a.append(5)
+# print(a)
+
+# a.remove(5)  # item
+# a.pop(1)  # index
+
+# print(a)
+
+# import array as arr
+
+# a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# b = arr.array("i", a)
+
+# res = a[3:8]
+# print(res)
+
+# res = a[5:]
+# print(res)
+
+# res = a[:]
+# print(res)
+
+
+# res = a[::-1]
+# print(res)
+
+
+# kw = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5}
+# print(kw.get(6, 0))
+# print(kw.popitem())
+# print(kw)
